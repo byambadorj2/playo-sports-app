57b4da27860d58b9ba81a48ead1a1032100bd423
byambadorj2 <byambadorj993@gmail.com>
2024-12-07 18:30:29 +0800

    add BottomNavBar

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import BookScreen from '../screens/BookScreen';
+import PlayScreen from '../screens/PlayScreen';
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HOME"
+          component={HomeScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="home-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="PLAY"
+          component={PlayScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            headerShown: false,
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <AntDesign name="addusergroup" size={24} color="green" />
+              ) : (
+                <AntDesign name="addusergroup" size={24} color="#989898" />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="BOOK"
+          component={BookScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            headerShown: false,
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <SimpleLineIcons name="book-open" size={24} color="green" />
+              ) : (
+                <SimpleLineIcons name="book-open" size={24} color="#989898" />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="PROFILE"
+          component={ProfileScreen}
+          options={{
+            tabBarActiveTintColor: 'green',
+            headerShown: false,
+            tabBarIcon: ({focused}) =>
+              focused ? (
+                <Ionicons name="person-outline" size={24} color="green" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="#989898" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+  const AuthStack = () => {
+    return <Stack.Navigator></Stack.Navigator>;
+  };
+
+  function MainStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
