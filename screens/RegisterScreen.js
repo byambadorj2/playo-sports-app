a8d65f76258954f42a00780bb4dfb72735ea1f2e
byambadorj2 <byambadorj993@gmail.com>
2024-12-11 18:53:54 +0800

    design the register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,11 +1,66 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState('');
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{padding: 13}}>
+        <Text style={{fontSize: 16, fontWeight: '500'}}>
+          You're Almost There
+        </Text>
+        <View style={{flexDirection: 'column', gap: 16, marginVertical: 40}}>
+          <Text style={{fontSize: 16, color: 'gray'}}>Enter Email</Text>
+
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            style={{
+              padding: 15,
+              borderColor: '#D0D0D0',
+              borderWidth: 1,
+              borderRadius: 10,
+            }}
+          />
+
+          <Pressable
+            onPress={() => navigation.navigate('Password')}
+            style={{
+              padding: 15,
+              backgroundColor: email?.length > 4 ? '#2dcf30' : '#E0E0E0',
+              borderRadius: 8,
+            }}>
+            <Text style={{textAlign: 'center'}}>Next</Text>
+          </Pressable>
+        </View>
+
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{textAlign: 'center', fontWeight: '500', fontSize: 15}}>
+            I agree to receive updates over Whatsapp
+          </Text>
+
+          <Text
+            style={{
+              fontSize: 15,
+              color: 'gray',
+              textAlign: 'center',
+              marginTop: 20,
+            }}>
+            By Signing up, you agree to the Terms of Service and Privacy and
+            Privacy Policy
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
